60bf26e80335c6e7bbcdf4450a2a857a61606cbe
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-08-07 14:10:21 -0700

    finish direction check

--- pacman.py
+++ pacman.py
@@ -76,8 +76,13 @@ def check_position(centerx, centery):
     turns = [False, False, False, False]
     num1 = (HEIGHT-50)//32
     num2 = (WIDTH//30)
-    num3 = 15
+    #checks width of body size
+    num3 = 15 
+    
+
+
     if centerx //30 < 29:
+        #check player location
         if direction == 0:
             if level[centery//num1][(centerx - num3)// num2] < 3:
                 turns[1] = True
@@ -85,14 +90,38 @@ def check_position(centerx, centery):
             if level[centery//num1][(centerx + num3)// num2] < 3:
                 turns[0] = True
         if direction == 2:
-            if level[(centery+num3)//num1][(centerx)// num2] < 3:
+            if level[(centery + num3)//num1][(centerx)// num2] < 3:
                 turns[3] = True
         if direction == 3:
-            if level[(centery-num3)//num1][(centerx)// num2] < 3:
+            if level[(centery - num3)//num1][(centerx)// num2] < 3:
                 turns[2] = True
 
+        #check if can move up or down
         if direction == 2 or direction == 3:
-            pass
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num3)//num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num3)//num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level[(centery//num1)][(centerx - num2)// num2] < 3:
+                    turns[1] = True
+                if level[(centery//num1)][(centerx + num2) // num2] < 3:
+                    turns[0] = True
+
+        #check if can move right or left
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num1)//num1][centerx // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num1)//num1][centerx // num2] < 3:
+                    turns[2] = True
+            if 12 <= centery % num1 <= 18:
+                if level[(centery//num1)][(centerx - num3)// num2] < 3:
+                    turns[1] = True
+                if level[(centery//num1)][(centerx + num3) // num2] < 3:
+                    turns[0] = True
+
     else:
         turns[0] = True
         turns[1]= True
