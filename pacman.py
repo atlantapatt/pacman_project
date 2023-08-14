726d0fe438f1a2ef45065b5ed1be161dcc58d332
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-08-14 15:51:38 -0700

    continue ghost movement if ghost cannot move left

--- pacman.py
+++ pacman.py
@@ -187,6 +187,7 @@ class Ghost:
         return self.turns, self.in_box 
 
     def move_orange(self):
+        #direction for moving right
         if self.direction == 0:
             if self.target[0] > self.x_pos and self.turns[0]:
                 self.x_pos += self.speed
@@ -194,21 +195,65 @@ class Ghost:
                 if self.target[1] > self.y_pos and self.turns[3]:
                     self.direction = 3
                     self.y_pos += self.speed
-            elif self.target[1] < self.y_pos and self.turns[2]:
-                self.direction = 2
-                self.y_pos -= self.speed
-            elif self.target[0] < self.x_pos and self.turns[1]:
-                self.direction = 1
-                self.x_pos -= self.speed
-            elif self.turns[3]:
-                self.direction = 3
-                self.y_pos += self.speed
-            elif self.turns[2]:
-                self.direction = 2
-                self.y_pos -= self.speed
-            elif self.turns[1]:
-                self.direction = 1
-                self.x_pos -= self.speed
+                elif self.target[1] < self.y_pos and self.turns[2]:
+                    self.direction = 2
+                    self.y_pos -= self.speed
+                elif self.target[0] < self.x_pos and self.turns[1]:
+                    self.direction = 1
+                    self.x_pos -= self.speed
+                elif self.turns[3]:
+                    self.direction = 3
+                    self.y_pos += self.speed
+                elif self.turns[2]:
+                    self.direction = 2
+                    self.y_pos -= self.speed
+                elif self.turns[1]:
+                    self.direction = 1
+                    self.x_pos -= self.speed
+            elif self.turns[0]:
+                if self.target[1] > self.y_pos and self.turns[3]:
+                    self.direction = 3
+                    self.y_pos += self.speed
+                if self.target[1] > self.y_pos and self.turns[2]:
+                    self.direction = 2
+                    self.y_pos -= self.speed
+                else:
+                    self.x_pos += self.speed
+
+        elif self.direction == 1:
+                if self.target[1] > self.y_pos and self.turns[3]:
+                    self.direction = 3
+                elif self.target[0] < self.x_pos and self.turns[1]:
+                    self.x_pos += self.speed
+                elif not self.turns[1]:
+                    if self.target[1] > self.y_pos and self.turns[3]:
+                        self.direction = 3
+                        self.y_pos += self.speed
+                    elif self.target[1] < self.y_pos and self.turns[2]:
+                        self.direction = 2
+                        self.y_pos -= self.speed
+                    elif self.target[0] > self.x_pos and self.turns[0]:
+                        self.direction = 0
+                        self.x_pos += self.speed
+                    elif self.turns[3]:
+                        self.direction = 3
+                        self.y_pos += self.speed
+                    elif self.turns[2]:
+                        self.direction = 2
+                        self.y_pos -= self.speed
+                    elif self.turns[0]:
+                        self.direction = 0
+                        self.x_pos += self.speed
+                elif self.turns[1]:
+                    if self.target[1] > self.y_pos and self.turns[3]:
+                        self.direction = 3
+                        self.y_pos += self.speed
+                    if self.target[1] > self.y_pos and self.turns[2]:
+                        self.direction = 2
+                        self.y_pos -= self.speed
+                    else:
+                        self.x_pos -= self.speed
+
 
 def draw_misc():
     score_text = font.render(f'Score: {score}', True, 'white')
